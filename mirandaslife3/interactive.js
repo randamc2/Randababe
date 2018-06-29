8c4d4c013ace33fba89d1e0d7af300bb696e2d99
randamc2 <40700303+randamc2@users.noreply.github.com>
2018-06-29 13:57:17 -0400

    Add files via upload

---
+++ mirandaslife3/interactive.js
@@ -0,0 +1,10 @@
+
+
+
+
+function afnctn() {
+   var btn = document.createElement("BUTTON");
+   var b = document.createTextNode("Miranda was president of the beta club in 5th grade");
+   btn.appendChild(b);
+  document.body.appendChild(btn);
+ }
